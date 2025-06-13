cf8e7142f23e5c2888cd9242a7349b86ca6ee02c
Wagner Palmeira <wagnerpalmeira.ti@gmail.com>
2025-06-12 23:57:16 -0300

    Feat: Adding array manipulation

---
+++ basic/array/target/debug/array.d
@@ -0,0 +1,1 @@
+/home/ownard/workspace/rust/rust-common/basic/array/target/debug/array: /home/ownard/workspace/rust/rust-common/basic/array/src/main.rs
---
+++ basic/array/target/debug/deps/array-7aa9ae1b29356935.d
@@ -0,0 +1,5 @@
+/home/ownard/workspace/rust/rust-common/basic/array/target/debug/deps/array-7aa9ae1b29356935: src/main.rs
+
+/home/ownard/workspace/rust/rust-common/basic/array/target/debug/deps/array-7aa9ae1b29356935.d: src/main.rs
+
+src/main.rs:
